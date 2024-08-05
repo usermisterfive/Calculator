aa5b5f2d0ecd40dd65075cb04a1ce3e22bbb1a75
usermisterfive <usermisterfive@yandex.ru>
2024-08-05 08:36:44 +0400

    #2. add reset C button.

--- app/src/main/java/com/calculator/MainActivity.java
+++ app/src/main/java/com/calculator/MainActivity.java
@@ -16,59 +16,107 @@ public class MainActivity extends AppCompatActivity {
   super.onCreate(savedInstanceState);
   setContentView(R.layout.activity_main);
   ((TextView) findViewById(R.id.appVersionTextView)).setText("v" + version);
-  TextView helloView = findViewById(R.id.resultTextView);
-  helloView.setText("0");
+  TextView expressionTextView = findViewById(R.id.expressionTextView);
+  expressionTextView.setText("0");
   findViewById(R.id.oneButton).setOnClickListener((View v) -> {
    Integer digit = 1;
-   if(helloView.getText().equals("0")) { helloView.setText(digit.toString()); }
-   else { helloView.setText(helloView.getText() + digit.toString()); }});
+   if (expressionTextView.getText().equals("0")) {
+    expressionTextView.setText(digit.toString());
+   }
+   else {
+    expressionTextView.setText(expressionTextView.getText() + digit.toString());
+   }});
   findViewById(R.id.twoButton).setOnClickListener((View v) -> {
    Integer digit = 2;
-   if(helloView.getText().equals("0")) { helloView.setText(digit.toString()); }
-   else { helloView.setText(helloView.getText() + digit.toString()); }});
+   if (expressionTextView.getText().equals("0")) {
+    expressionTextView.setText(digit.toString());
+   }
+   else {
+    expressionTextView.setText(expressionTextView.getText() + digit.toString());
+   }});
   findViewById(R.id.threeButton).setOnClickListener((View v) -> {
    Integer digit = 3;
-   if(helloView.getText().equals("0")) { helloView.setText(digit.toString()); }
-   else { helloView.setText(helloView.getText() + digit.toString()); }});
+   if (expressionTextView.getText().equals("0")) {
+    expressionTextView.setText(digit.toString());
+   }
+   else {
+    expressionTextView.setText(expressionTextView.getText() + digit.toString());
+   }});
   findViewById(R.id.fourButton).setOnClickListener((View v) -> {
    Integer digit = 4;
-   if(helloView.getText().equals("0")) { helloView.setText(digit.toString()); }
-   else { helloView.setText(helloView.getText() + digit.toString()); }});
+   if (expressionTextView.getText().equals("0")) {
+    expressionTextView.setText(digit.toString());
+   }
+   else {
+    expressionTextView.setText(expressionTextView.getText() + digit.toString());
+   }});
   findViewById(R.id.fiveButton).setOnClickListener((View v) -> {
    Integer digit = 5;
-   if(helloView.getText().equals("0")) { helloView.setText(digit.toString()); }
-   else { helloView.setText(helloView.getText() + digit.toString()); }});
+   if (expressionTextView.getText().equals("0")) {
+    expressionTextView.setText(digit.toString());
+   }
+   else {
+    expressionTextView.setText(expressionTextView.getText() + digit.toString());
+   }});
   findViewById(R.id.sixButton).setOnClickListener((View v) -> {
    Integer digit = 6;
-   if(helloView.getText().equals("0")) { helloView.setText(digit.toString()); }
-   else { helloView.setText(helloView.getText() + digit.toString()); }});
+   if (expressionTextView.getText().equals("0")) {
+    expressionTextView.setText(digit.toString());
+   }
+   else {
+    expressionTextView.setText(expressionTextView.getText() + digit.toString());
+   }});
   findViewById(R.id.sevenButton).setOnClickListener((View v) -> {
    Integer digit = 7;
-   if(helloView.getText().equals("0")) { helloView.setText(digit.toString()); }
-   else { helloView.setText(helloView.getText() + digit.toString()); }});
+   if (expressionTextView.getText().equals("0")) {
+    expressionTextView.setText(digit.toString());
+   }
+   else {
+    expressionTextView.setText(expressionTextView.getText() + digit.toString());
+   }});
   findViewById(R.id.eightButton).setOnClickListener((View v) -> {
    Integer digit = 8;
-   if(helloView.getText().equals("0")) { helloView.setText(digit.toString()); }
-   else { helloView.setText(helloView.getText() + digit.toString()); }});
+   if (expressionTextView.getText().equals("0")) {
+    expressionTextView.setText(digit.toString());
+   }
+   else {
+    expressionTextView.setText(expressionTextView.getText() + digit.toString());
+   }});
   findViewById(R.id.nineButton).setOnClickListener((View v) -> {
    Integer digit = 9;
-   if(helloView.getText().equals("0")) { helloView.setText(digit.toString()); }
-   else { helloView.setText(helloView.getText() + digit.toString()); }});
+   if (expressionTextView.getText().equals("0")) {
+    expressionTextView.setText(digit.toString());
+   }
+   else {
+    expressionTextView.setText(expressionTextView.getText() + digit.toString());
+   }});
   findViewById(R.id.zeroButton).setOnClickListener((View v) -> {
    Integer digit = 0;
-   if(helloView.getText().equals("0")) { helloView.setText(digit.toString()); }
-   else { helloView.setText(helloView.getText() + digit.toString()); }});
+   if (expressionTextView.getText().equals("0")) {
+    expressionTextView.setText(digit.toString());
+   }
+   else {
+    expressionTextView.setText(expressionTextView.getText() + digit.toString());
+   }});
   findViewById(R.id.plusButton).setOnClickListener((View v) -> {
-   helloView.setText(helloView.getText() + "+");
+   expressionTextView.setText(expressionTextView.getText() + "+");
   });
   findViewById(R.id.equalsButton).setOnClickListener((View v) -> {
-   String expression = helloView.getText().toString();
-   if(expression.contains("+")) {
+   String expression = expressionTextView.getText().toString();
+   if (expression.contains("+")) {
     List<String> numbers = Arrays.asList(expression.split("[+]"));
+    String expression2 = "=";
+    Integer number0 = 0;
+    for (String number : numbers) {
+     number0 = (number0 + Integer.parseInt(number));
+    }
+    expression2 = expression2 + number0;
     ((TextView) findViewById(R.id.equalsTextView))
-      .setText("=" + (Integer.parseInt(numbers.get(0)) + (Integer.parseInt(numbers.get(1)))));
+      .setText(expression2);
    }
-
+  });
+  findViewById(R.id.resetButton).setOnClickListener((View v) -> {
+   expressionTextView.setText("");
   });
  }
 }
