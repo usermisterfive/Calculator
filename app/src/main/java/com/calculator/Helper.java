dcc78dfea60620419d515e8f3f222024e33c21f3
usermisterfive <usermisterfive@yandex.ru>
2025-04-01 14:24:55 +0400

    use bracket expression replacement in MainActivity #4

--- app/src/main/java/com/calculator/Helper.java
+++ app/src/main/java/com/calculator/Helper.java
@@ -27,7 +27,7 @@ public class Helper {
    String stringCharacter = String.valueOf(chars[iteration]);
    if (isParsableDouble(stringCharacter) || stringCharacter.equals(".")) {
     if (iteration == 0) {
-     symbols.add(Symbols.PLUS.getSymbol());
+     symbols.add(Symbols.PLUS.toString());
     }
     if (stringNumbers.size() < numberId + 1) {
      stringNumbers.add("");
@@ -53,11 +53,11 @@ public class Helper {
    List<String> numbers = list.get(0);
    List<String> symbols = list.get(1);
    List<Double> expression2 = new ArrayList<>();
-   multiplyDivide(numbers, symbols, Symbols.MULTIPLY.getSymbol());
-   multiplyDivide(numbers, symbols, Symbols.DIVIDE.getSymbol());
+   multiplyDivide(numbers, symbols, Symbols.MULTIPLY.toString());
+   multiplyDivide(numbers, symbols, Symbols.DIVIDE.toString());
    for (int iteration = 0; iteration < numbers.size(); iteration++) {
     Double number = Double.parseDouble(numbers.get(iteration));
-    if (symbols.get(iteration).equals(Symbols.MINUS.getSymbol())) {
+    if (symbols.get(iteration).equals(Symbols.MINUS.toString())) {
      number = - number;
     }
     expression2.add(number);
@@ -87,10 +87,10 @@ public class Helper {
    if (symbols.contains(symbol)) {
     int index = symbols.indexOf(symbol);
     Double numbersOperated = Double.NaN;
-    if (symbol.equals(Symbols.MULTIPLY.getSymbol())) {
+    if (symbol.equals(Symbols.MULTIPLY.toString())) {
      numbersOperated = Double.parseDouble(numbers.get(index))
        * Double.parseDouble(numbers.get(index - 1));
-    } else if (symbol.equals(Symbols.DIVIDE.getSymbol())) {
+    } else if (symbol.equals(Symbols.DIVIDE.toString())) {
      numbersOperated = Double.parseDouble(numbers.get(index - 1)) /
        Double.parseDouble(numbers.get(index));
     }
@@ -103,28 +103,18 @@ public class Helper {
  static String removeDuplicateOperator(TextView expressionTextView) {
   String expression = expressionTextView.getText().toString();
   for (Symbols symbol : Symbols.values()) {
-   if (expression.endsWith(symbol.getSymbol())) {
+   if (expression.endsWith(symbol.toString())) {
     expression = expression.substring(0, expression.length() - 1);
    }
   }
   return expression;
  }
 
- /**
-  * In this implementation works only with one left bracket and one right bracket.
-  * @param expression to process
-  * @return modified expression
-  */
  static String replaceBracketExpression(String expression) {
-  String expression2 = expression;
-  if (expression.chars().filter(ch -> ch == '(').count() == 1
-    && expression.chars().filter(ch -> ch == ')').count() == 1) {
-   expression2 = expression.replace(
-     expression.substring(expression.indexOf("("), expression.indexOf(")") + 1),
-     String.valueOf(calculate(parse(
-       expression.substring(expression.indexOf("(") + 1, expression.indexOf(")"))))));
-  }
-  return expression2;
+  return expression.replace(
+    expression.substring(expression.indexOf("("), expression.indexOf(")") + 1),
+    String.valueOf(calculate(parse(
+      expression.substring(expression.indexOf("(") + 1, expression.indexOf(")"))))));
  }
 
 
